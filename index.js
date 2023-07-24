21598dceecee0b6043fe7d8782f0cebfb11b7a3d
labankibet <laban.kibet@moringaschool.com>
2023-07-24 20:55:53 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Laban Kibet',
+    streetAddress: 'Nairobi',
+  };
+  
+  function updateEmployeeWithKeyAndValue(employeeObj, key, value) {
+    return {
+      ...employeeObj,
+      [key]: value,
+    };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employeeObj, key, value) {
+    employeeObj[key] = value;
+    return employeeObj;
+  }
+  
+  function deleteFromEmployeeByKey(employeeObj, key) {
+    const newEmployee = { ...employeeObj };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employeeObj, key) {
+    delete employeeObj[key];
+    return employeeObj;
+  }
+  
+  console.log('Original employee:', employee);
+  
+  let updatedEmployee = updateEmployeeWithKeyAndValue(employee, 'age', 23);
+  console.log('Updated employee (without mutation):', updatedEmployee);
+  
+  destructivelyUpdateEmployeeWithKeyAndValue(employee, 'age', 23);
+  console.log('Original employee (mutated):', employee);
+  
+  let employeeWithoutStreet = deleteFromEmployeeByKey(employee, 'streetAddress');
+  console.log('Employee without streetAddress (without mutation):', employeeWithoutStreet);
+  
+  destructivelyDeleteFromEmployeeByKey(employee, 'streetAddress');
+  console.log('Original employee without streetAddress (mutated):', employee);
+  
